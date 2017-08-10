8e46bdc49573d82e901f5c57ff1928bae9e82593
Rafael K. Peres <rafaelkperes@gmail.com>
2017-08-10 14:22:29 -0300

    Add canvas and draw rectangles
    Add a canvas to index.html. Build a class for drawing rectangles.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,31 @@
-console.log('\'Allo \'Allo!');
+const pxcanvasid = 'pixelcross-canvas';
+var   pxState;
+
+$( document ).ready(function() {
+  pxState = new Pixelcross(pxcanvasid);
+  pxState.init();
+});
+
+var Pixelcross = function(canvasid) {
+  this.canvas  = undefined;
+  this.context = undefined;
+
+  this.init = function() {
+    this.canvas  = document.getElementById(canvasid);
+    this.context = this.canvas.getContext('2d');
+
+    var ctx = this.context;
+
+    ctx.fillStyle = 'green';
+    ctx.fillRect(30, 30, 20, 20);
+
+    ctx.fillStyle = 'purple';
+    ctx.fillRect(60, 60, 20, 20);
+
+    ctx.strokeStyle = 'darkgreen';
+    ctx.strokeRect(60, 60, 20, 20);
+
+    ctx.strokeStyle = 'black';
+    ctx.strokeRect(90, 90, 20, 20);
+  };
+}
