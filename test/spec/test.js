0745ad6d47db700c8a0170170e36d37f23442779
Rafael K. Peres <rafaelkperes@gmail.com>
2017-08-10 15:25:15 -0300

    Test pixel further
    Add needed tests for Pixel state cycling.

--- test/spec/test.js
+++ test/spec/test.js
@@ -7,6 +7,20 @@
         var pixel = new Pixel()
         expect(pixel.state).to.equal(PixelState.EMPTY)
       });
+
+      it('cicles correctly between states', function () {
+        var pixel = new Pixel()
+        expect(pixel.state).to.equal(PixelState.EMPTY)
+
+        pixel.nextState()
+        expect(pixel.state).to.equal(PixelState.FILLED)
+
+        pixel.nextState()
+        expect(pixel.state).to.equal(PixelState.CROSSED)
+
+        pixel.nextState()
+        expect(pixel.state).to.equal(PixelState.EMPTY)
+      });
     });
   });
 })();
